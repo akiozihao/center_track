c684ea30e9afe3e4c41dbfe5da6133fa02f21d1d
akiozihao <akiozihaoceat@gmail.com>
2021-07-27 22:12:27 +0800

    change tracker

--- mmtrack/models/mot/trackers/ct_tracker.py
+++ mmtrack/models/mot/trackers/ct_tracker.py
@@ -14,12 +14,16 @@ class CTTracker(BaseTracker):
         super(CTTracker, self).__init__(**kwargs)
         self.pre_img = None
         self.pre_centers = None
-        self.pre_center2id = {}
+        self.pre_bboxes = None
+        self.pre_labels = None
+        self.obj_score_thr = obj_score_thr
 
     def reset(self):
         super(CTTracker, self).reset()
         self.pre_img = None
         self.pre_centers = None
+        self.pre_bboxes = None
+        self.pre_labels = None
 
     def track(self,
               img,  # todo
@@ -30,6 +34,11 @@ class CTTracker(BaseTracker):
               frame_id,
               rescale,  # todo
               **kwargs):
+        valid_inds = bboxes[:, -1] > self.obj_score_thr
+        bboxes = bboxes[valid_inds]
+        bboxes_with_motion = bboxes_with_motion[valid_inds]
+        labels = labels[valid_inds]
+
         if self.empty or bboxes.size(0) == 0:
             num_new_tracks = bboxes.size(0)
             ids = torch.arange(
@@ -38,13 +47,25 @@ class CTTracker(BaseTracker):
                 dtype=torch.long)
             self.num_tracks += num_new_tracks
             self.pre_img = img
+            self.pre_bboxes = bboxes
             self.pre_centers = self._xyxy2center(bboxes)
+            self.pre_labels = labels
         else:
             ids = torch.full((bboxes.size(0),), -1, dtype=torch.long)
-
+            M = self.pre_bboxes.shape[0]
+            N = bboxes.shape[0]
+            track_size = (self.pre_bboxes[:, 3] - self.pre_bboxes[:, 1]) * \
+                         (self.pre_bboxes[:, 2] - self.pre_bboxes[:, 0])  # M
+            item_size = (bboxes[:, 3] - bboxes[:, 1]) * (bboxes[:, 2] - bboxes[:, 0])  # N
             det_centers = self._xyxy2center(bboxes_with_motion)
-            dist = torch.cdist(det_centers, self.pre_centers, 2)  # todo ximi cheack : diff with origin
+            dist = torch.cdist(det_centers, self.pre_centers, 2)
+            # invalid
+            invalid = ((dist > track_size.reshape(1, M)) + \
+                       (dist > item_size.reshape(N, 1)) + \
+                       (labels.reshape(N, 1) != self.pre_labels.reshape(1, M))) > 0
+            dist = dist + invalid * 1e18
             matched_indices = self._greedy_assignment(dist)
+
             for i in range(matched_indices.shape[0]):
                 ids[matched_indices[i][0]] = matched_indices[i][1]
 
@@ -56,7 +77,8 @@ class CTTracker(BaseTracker):
             self.num_tracks += new_track_inds.sum()
             self.pre_img = img
             self.pre_centers = det_centers
-
+            self.pre_bboxes = bboxes
+            self.pre_labels = labels
         self.update(
             ids=ids,
             bboxes=bboxes,
